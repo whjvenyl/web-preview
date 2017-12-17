996291557e70e3432263aace37dbca6e7da622ad
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-12-16 22:48:06 -0500

    Update 2017-12-17T03:48:03.080Z

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp /etc/bitwarden/web/settings.js /app/js/settings.js
-cp /etc/bitwarden/web/app-id.json /app/app-id.json
-dotnet /bitwarden_server/Server.dll /contentRoot=/app /webRoot=. /serveUnknown=false
--- src/app/settings.js
+++
@@ -1,2 +0,0 @@
-angular.module("bit")
-.constant("appSettings", {"apiUri":"/api","identityUri":"/identity","iconsUri":"https://icons.bitwarden.com","stripeKey":"pk_live_bpN0P37nMxrMQkcaHXtAybJk","braintreeKey":"production_qfbsv8kc_njj2zjtyngtjmbjd","whitelistDomains":["vault.bitwarden.com"],"selfHosted":false,"version":"1.21.0","environment":"Production"});
